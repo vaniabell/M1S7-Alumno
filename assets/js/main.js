9b0eccb1469d729bfba8ddc692c644a0a843f1bb
vaniabell <108189253+vaniabell@users.noreply.github.com>
2022-08-13 10:49:35 -0500

    Update main.js
    agregar let

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
 console.log("Entro al main.js");
+
+var nombre =" vania "
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+console.log(nombre)
+
+var nombre = "jaaciel";
+
+
+console.log(nombre)
+
+//aqui vamos usar let
+
+let nombre = "vania";
+let nombre = "isabel";
+
+console.log(nombre_persona);
+console.log(nombre_persona);
+
